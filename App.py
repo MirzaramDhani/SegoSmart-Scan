a259af0ebbe0cdc54748bba820015d945060b020
Achmad Mirazaram Dhani <143950513+MirzaramDhani@users.noreply.github.com>
2025-06-23 11:32:50 +0700

    opencv headless

--- App.py
+++ App.py
@@ -6,38 +6,26 @@ from Expression_Detector import detect_expression
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "https://sego-smart-fe.vercel.app"}}, supports_credentials=True)
 
-# Log saat server startup
-print("Flask Server is running and ready to receive requests...")
-
 # Endpoint untuk mengunggah gambar dan mendapatkan rating kepuasan
+
 @app.route("/analyze", methods=["POST"])
 def analyze_image():
-    print("Received request at /analyze")
-
     if 'file' not in request.files:
-        print("Tidak ada file di request")
         return jsonify({"error": "Tidak ada file yang diunggah"}), 400
-
+    
     file = request.files['file']
     file_path = "temp.jpg"
     file.save(file_path)
-    print(f"File berhasil disimpan sementara sebagai {file_path}")
 
-    try:
-        # Analisis ekspresi menggunakan fungsi di expression_detector.py
-        result = detect_expression(file_path)
-        print(" Ekspresi berhasil dianalisis:", result)
-    except Exception as e:
-        print(" Gagal menganalisis ekspresi:", str(e))
-        result = {"error": "Gagal menganalisis ekspresi"}
-
+    # Analisis ekspresi menggunakan fungsi di expression_detector.py
+    result = detect_expression(file_path)
+    print(result)
+    
     # Hapus gambar sementara setelah analisis
-    if os.path.exists(file_path):
-        os.remove(file_path)
-        print(f" File sementara {file_path} dihapus")
-
+    os.remove(file_path)
+    
     return jsonify(result)
 
 if __name__ == "__main__":
-    print(" Memulai Flask server...")
+    print("Menjalankan Server Scan Mood")
     app.run(debug=True)
